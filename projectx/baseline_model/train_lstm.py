67ed65cf75c58e9149ddd73cc255bf53e4b56021
yuchenWYC <raina.wang@mail.utoronto.ca>
2020-11-23 00:59:01 -0500

    1. devided loss by number of batches 2. dropped the last batch

--- projectx/baseline_model/train_lstm.py
+++ projectx/baseline_model/train_lstm.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 import argparse
 import pandas as pd
 from hyperparams import Hyperparameters
+
 # multivariate data preparation
 from lstm import MV_LSTM
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -18,35 +20,45 @@ def parse_args() -> argparse.Namespace:
 
     return parser.parse_args()
 
+
 def get_hyperparameters(args: argparse.Namespace) -> Hyperparameters:
     return Hyperparameters(
         lr=args.lr,
-        batch_size = args.batch_size,
+        batch_size=args.batch_size,
         variance=0.1,
         sequence_length=args.sequence_length,
         num_epochs=args.num_epochs,
     )
 
+
 def split_sequences(seq, n_steps):
     X, y = [], []
     for i in range(len(seq) - n_steps - 1):
-        seq_x, seq_y = seq[i:(i + n_steps), :], seq[i + n_steps, 3]
+        seq_x, seq_y = seq[i : (i + n_steps), :], seq[i + n_steps, 3]
         X.append(seq_x)
         y.append(seq_y)
     return np.array(X), np.array(y)
 
-def train(path, save_path, n_features = 4,n_timesteps = 100, train_episodes = 256, batch_size = 256, lr=0.001):
+
+def train(
+    path,
+    save_path,
+    n_features=4,
+    n_timesteps=100,
+    train_episodes=256,
+    batch_size=256,
+    lr=0.001,
+):
 
     df = pd.read_csv(path)
-    del df['date']
+    del df["date"]
     sq = df.to_numpy()
-    X, y = split_sequences(sq, n_steps = n_timesteps)
+    X, y = split_sequences(sq, n_steps=n_timesteps)
 
-    X_train, y_train = X[:int(len(X) * 0.9)], y[:int(len(X) * 0.9)]
-    X_test, y_test = X[int(len(X) * 0.9):], y[int(len(X) * 0.9):]
+    X_train, y_train = X[: int(len(X) * 0.9)], y[: int(len(X) * 0.9)]
+    X_test, y_test = X[int(len(X) * 0.9) :], y[int(len(X) * 0.9) :]
 
     mv_net = MV_LSTM(n_features, n_timesteps)
-    criterion = torch.nn.MSELoss(reduction='sum')  # reduction='sum' created huge loss value
     optimizer = torch.optim.Adam(mv_net.parameters(), lr=lr)
 
     # use GPU
@@ -59,17 +71,24 @@ def train(path, save_path, n_features = 4,n_timesteps = 100, train_episodes = 25
     val_loss_plot = []
     for t in range(train_episodes):
         step_loss = 0
-        count = 0
         for b in range(0, len(X_train), batch_size):
-            count += 1
-            inpt = X_train[b:b + batch_size, :, :]  # /np.linalg.norm(X_train[b:b+batch_size,:,:])
-            target = y_train[b:b + batch_size]  # /np.linalg.norm(y_train[b:b+batch_size])
+            if b + batch_size >= len(X_train):
+                break
+            inpt = X_train[
+                b : b + batch_size, :, :
+            ]  # /np.linalg.norm(X_train[b:b+batch_size,:,:])
+            target = y_train[
+                b : b + batch_size
+            ]  # /np.linalg.norm(y_train[b:b+batch_size])
             if target.shape[0] != 0:
-                x_batch = torch.from_numpy(inpt).float().cuda()  # torch.tensor(inpt,dtype=torch.float32)
-                y_batch = torch.from_numpy(target).float()  # torch.tensor(target,dtype=torch.float32)
-                mv_net.init_hidden(x_batch.size(0))
+                x_batch = (
+                    torch.from_numpy(inpt).float().to(device)
+                )  # torch.tensor(inpt,dtype=torch.float32)
+                y_batch = torch.from_numpy(
+                    target
+                ).float()  # torch.tensor(target,dtype=torch.float32)
                 output = mv_net(x_batch)
-                #loss = criterion(output.cpu().view(-1), np.transpose(y_batch))
+                # loss = criterion(output.cpu().view(-1), np.transpose(y_batch))
                 infect_dist = torch.distributions.normal.Normal(y_batch, 0.1)
                 loss = -infect_dist.log_prob(output.squeeze().cpu()).mean()
                 loss.backward()
@@ -81,27 +100,36 @@ def train(path, save_path, n_features = 4,n_timesteps = 100, train_episodes = 25
         val_loss = 0
         with torch.no_grad():
             for b in range(0, len(X_test), batch_size):
-                test_seq = X_test[b:b + batch_size, :, :]  # /np.linalg.norm(X_test)
-                label_seq = y_test[b:b + batch_size]  # /np.linalg.norm(y_test[b:b+batch_size])
-                x_batch = torch.from_numpy(test_seq).float().cuda()
+                if b + batch_size >= len(X_test):
+                    break
+                test_seq = X_test[b : b + batch_size, :, :]  # /np.linalg.norm(X_test)
+                label_seq = y_test[
+                    b : b + batch_size
+                ]  # /np.linalg.norm(y_test[b:b+batch_size])
+                x_batch = torch.from_numpy(test_seq).float().to(device)
                 y_batch = torch.from_numpy(label_seq).float()
-                mv_net.init_hidden(x_batch.size(0))
                 try:
                     output = mv_net(x_batch)
-                    #batch_val_loss = criterion(output.cpu().view(-1), np.transpose(y_batch))
+                    # batch_val_loss = criterion(output.cpu().view(-1), np.transpose(y_batch))
                     infect_dist = torch.distributions.normal.Normal(y_batch, 0.1)
-                    batch_val_loss = -infect_dist.log_prob(output.squeeze().cpu()).mean()
+                    batch_val_loss = -infect_dist.log_prob(
+                        output.squeeze().cpu()
+                    ).mean()
                     val_loss += batch_val_loss.item()
                 except:
                     continue
-        loss_plot.append(step_loss / len(X_train))
-        val_loss_plot.append(val_loss / len(X_test))
-        if val_loss / len(X_test) < best_loss:
+        num_batches_train = len(X_train) // batch_size
+        num_batches_test = len(X_test) // batch_size
+        train_loss = step_loss / num_batches_train
+        valid_loss = val_loss / num_batches_test
+        loss_plot.append(train_loss)
+        val_loss_plot.append(valid_loss)
+        if valid_loss < best_loss:
             torch.save(mv_net.state_dict(), save_path)
-            best_loss = val_loss / len(X_test)
+            best_loss = valid_loss
 
-        print('step : ', t, 'training loss : ', step_loss / len(X_train))
-        print('step : ', t, 'validation loss : ', val_loss / len(X_test))
+        print("step : ", t, "training loss : ", train_loss)
+        print("step : ", t, "validation loss : ", valid_loss)
 
     updates = [i for i in range(1, len(loss_plot) + 1)]
     plt.plot(updates, loss_plot, label="Training loss")
@@ -110,8 +138,9 @@ def train(path, save_path, n_features = 4,n_timesteps = 100, train_episodes = 25
     plt.xlabel("Steps")
     plt.ylabel("Loss")
     plt.legend()
-    plt.savefig('./MLE Curve for LSTM.jpg')
+    plt.savefig("./MLE Curve for LSTM.jpg")
     plt.show()
 
+
 if __name__ == "__main__":
-    train(path = './toy.csv', save_path= './lstm_state_dict.pt')
+    train(path="./toy.csv", save_path="./lstm_state_dict.pt")
