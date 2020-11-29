ee6ceaf99530090534cded24ace622dcb66d1291
yuchenWYC <raina.wang@mail.utoronto.ca>
2020-11-29 17:00:54 -0500

    update baseline sbatch

--- baseline/sbatch_baseline.py
+++ baseline/sbatch_baseline.py
@@ -19,7 +19,7 @@ if not job_dir.exists():
 
 # Hyperparams to try
 lr_list = [1e-4]
-hidden_dims_list = [30]
+hidden_dims_list = [20]
 seq_len_list = [100, 70, 50, 30]
 model_list = ['lstm', 'rnn']
 
@@ -55,23 +55,11 @@ with open("train_all.sh", "w") as allf:
                             f"#SBATCH --output='{job_out_file}'\n"
                         )
                         f.write(f"cd {root}\n")
-
-                        # Need to separate items in lists with spaces to pass them as args
-                        encoder_fc_dims_arg = " ".join(
-                            map(str, encoder_fc_dims)
-                        )
-                        odefunc_fc_dims_arg = " ".join(
-                            map(str, odefunc_fc_dims)
-                        )
-                        decoder_fc_dims_arg = " ".join(
-                            map(str, decoder_fc_dims)
-                        )
-
                         f.write(
                             f"python3 train_baseline.py "
                             + f"--lr={lr} "
-                            + f"--hidden_dims={hidden_dims} "
+                            + f"--n_hidden={hidden_dims} "
                             + f"--seq_len={seq_len} "
-                            + f"--model_name={model_name}"
+                            + f"--model_name={model}")
 
                     allf.write(f"sbatch '{job_file}'\n")
